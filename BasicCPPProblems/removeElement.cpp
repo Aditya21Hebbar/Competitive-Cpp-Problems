45246bf99b7cc57988d19ee2fe5b8d33a9785e8c
ADITYA-HEBBAR <84819735+Aditya21Hebbar@users.noreply.github.com>
2022-05-11 16:15:56 +0530

    changes

--- BasicCPPProblems/removeElement.cpp
+++ BasicCPPProblems/removeElement.cpp
@@ -8,12 +8,14 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        int index = 0;
-        for(int i: nums){
-            if(i !=val){
-               nums[index++] = i; 
+        int j = 0;
+        if(nums.size() ==  0) return 0;
+        for(int i =0;i<nums.size(); i++){
+            if(nums[i]!=val){
+                nums[j]=nums[i];
+                j++;
             }
         }
-        return index;
+        return j;
     }
 };
